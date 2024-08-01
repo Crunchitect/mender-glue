ef793075302f7c658ff8d2abe7a52e4584ccb567
PremPremPremPrem <prem.sy1952@gmail.com>
2024-08-01 21:19:44 +0700

    1.0.1

--- server.py
+++ server.py
@@ -3,25 +3,28 @@ import subprocess
 from websocket_server import WebsocketServer
 
 def new_client(client, server):
-    logging.info("New STL Slicer Called")
+    print("New STL Slicer Called")
 
 def message_received(client, server, message):
     stl = str(message)
+    print("RECIEVED:", stl)
     with open("in.stl", "w") as f:
         f.write(stl)
-    subprocess.Popen(['./cura/CuraEngine.exe',
+    subprocess.call(['./cura/CuraEngine.exe',
                       'slice',
-                      *['-j', './definintions/def.json'],
-                      *['-l', '../in.stl'],
-                      *['-o', '../out.gcode'],
+                      *['-j', './cura/definitions/def.json'],
+                      *['-l', './in.stl'],
+                      *['-o', './out.gcode'],
                       *['-s', 'roofing_layer_count=1']])
     output = ''
     with open("out.gcode", "r") as f:
         output = f.read()
+    print("SEND: ", output)
     server.send_message(client, output)
     logging.info("STL sliced.")
     
 
 server = WebsocketServer(host='127.0.0.1', port=1533, loglevel=logging.INFO)
 server.set_fn_new_client(new_client)
+server.set_fn_message_received(message_received)
 server.run_forever()
