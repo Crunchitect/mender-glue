dce4d5b9b63db92c1fb477b467f29b163486e6bc
PremPremPremPrem <prem.sy1952@gmail.com>
2024-08-02 16:34:21 +0700

    2.0.0 - Supports 648 printers!

--- server.py
+++ server.py
@@ -1,27 +1,39 @@
 import logging
 import subprocess
+import requests
+import json
 from websocket_server import WebsocketServer
 
 def new_client(client, server):
     print("New STL Slicer Called")
 
 def message_received(client, server, message):
-    stl = str(message)
-    print("RECIEVED:", stl)
-    with open("in.stl", "w") as f:
-        f.write(stl)
-    subprocess.call(['./cura/CuraEngine.exe',
-                      'slice',
-                      *['-j', './cura/definitions/def.json'],
-                      *['-l', './in.stl'],
-                      *['-o', './out.gcode'],
-                      *['-s', 'roofing_layer_count=1']])
-    output = ''
-    with open("out.gcode", "r") as f:
-        output = f.read()
-    print("SEND: ", output)
-    server.send_message(client, output)
-    logging.info("STL sliced.")
+    opcode, content = str(message).split('%', 1)
+    printer_name = 'anet3d_et4'
+    print("RECIEVED:", content)
+    match opcode:
+        case 'stl':
+            with open("in.stl", "w") as f:
+                f.write(content)
+            subprocess.call(['./cura/CuraEngine.exe',
+                            'slice',
+                            *['-j', f'./cura/definitions/{printer_name}.json'],
+                            *['-l', './in.stl'],
+                            *['-o', './out.gcode'],
+                            *['-s', 'roofing_layer_count=1']])
+            output = ''
+            with open("out.gcode", "r") as f:
+                output = f.read()
+            print("SEND: ", output)
+            server.send_message(client, output)
+            logging.info("STL sliced.")
+        case 'printer':
+            printer_name = content
+            server.send_message(client, 'ok')
+        case 'printerlist':
+            slug = requests.get("https://api.github.com/repos/ultimaker/cura/contents/resources/definitions")
+            printers = [printer['name'] for printer in slug.json()]
+            server.send_message(client, json.dumps(printers))
     
 
 server = WebsocketServer(host='127.0.0.1', port=1533, loglevel=logging.INFO)
